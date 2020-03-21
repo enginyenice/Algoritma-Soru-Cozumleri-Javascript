fc16cd3fe46cd6cfca7a09219d7c92b37f174b24
= <enginyenice2626@gmail.com>
2020-03-22 00:06:00 +0300

    1 ile 10 arasindaki tam sayilarin carpimini bulan algoritma

---
+++ ornek-2-5-12.js
@@ -0,0 +1,10 @@
+/*
+* 1 ile 10 arasindaki tam sayilarin carpimini bulan algoritma
+* */
+
+let toplam = 1;
+for(let i = 1;i < 10;i++)
+{
+    toplam *= i;
+}
+console.log("1 ile 10 arasindaki sayilarin carpimi: ",toplam);
