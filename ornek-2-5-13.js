f7d13eb8254d1f9be7788ad03e072ace5df23d6c
= <enginyenice2626@gmail.com>
2020-03-22 01:06:44 +0300

    Duzeltme

--- ornek-2-5-13.js
+++ ornek-2-5-13.js
@@ -4,11 +4,13 @@
 
 function asalMi(sayi)
 {
-    for(let i = 2; i< parseInt(sayi / 2); i++){
+    let i;
+    for(i = 2; i< parseInt(sayi / 2); i++){
         if(sayi / i - parseInt(sayi / i) === 0)
             return false
     }
     return true
 }
-for(let i = 10; i< 100; i++)
+let i;
+for(i = 10; i< 100; i++)
     (asalMi(i) === true) ? console.log("Asal Sayi: ", i) : "";
