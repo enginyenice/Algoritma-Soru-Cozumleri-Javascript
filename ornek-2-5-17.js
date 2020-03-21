b39e93ed19f073a934a10ae9f2036ccbad48e975
= <enginyenice2626@gmail.com>
2020-03-22 01:28:21 +0300

    Ondalikli kısmı iki haneli girilen pozitif bir rasyonel sayinin ondalikli kısmındaki sayı degerlerinin bir tam kare olup  olmadıgını arastır

---
+++ ornek-2-5-17.js
@@ -0,0 +1,8 @@
+/**
+ * Ondalikli kısmı iki haneli girilen pozitif bir rasyonel sayinin ondalikli kısmındaki sayı degerlerinin bir tam kare olup
+ * olmadıgını arastır
+ */
+let sayi = 17.123
+
+let tamKisim = parseInt(sayi)
+console.log((Math.pow(tamKisim, 0.5) == parseInt(Math.pow(tamKisim, 0.5))) ? "Tam kare" : "Tam kare degil")
