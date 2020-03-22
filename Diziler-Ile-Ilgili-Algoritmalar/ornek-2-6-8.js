7e279ce82fcdf270c1520b1654be9b995298e827
= <enginyenice2626@gmail.com>
2020-03-22 12:46:36 +0300

    10 elemanli bir diziyi kucukten buyuge dogru sıralama

--- Diziler-Ile-Ilgili-Algoritmalar/ornek-2-6-8.js
+++ Diziler-Ile-Ilgili-Algoritmalar/ornek-2-6-8.js
@@ -6,7 +6,7 @@ let dizi = [2, -16, 1, -9, 2, 7, -1, 66, 4, 2]
 let i = 0
 for (i; i < dizi.length; i++) {
     for (let k = i + 1; k < dizi.length; k++) {
-        if (dizi[i] < dizi[k]) {
+        if (dizi[i] > dizi[k]) {
             let tempSayi = dizi[k]
             dizi[k] = dizi[i]
             dizi[i] = tempSayi
